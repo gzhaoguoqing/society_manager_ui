0bcc32053e19bd714529ff73d845b0a1593a5d0e
zhaoguoqing <930574465@qq.com>
2019-05-22 23:51:52 +0800

    前端权限控制完成

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,6 @@ module.exports = {
   devServer: {
     port: 8010,
     proxy: {
-      // 请求到 '/device' 下 的请求都会被代理到 target： http://debug.xxx.com 中
       '/api/*': {
         target: 'http://localhost:8080',
         secure: false, // 接受 运行在 https 上的服务
